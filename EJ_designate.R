7106586ca83c33d5237fce83bfb9cd62f475a56e
unknown <marcos@starluna.net>
2021-12-05 17:24:04 -0500

    Added policy for de-designation

--- EJ_designate.R
+++ EJ_designate.R
@@ -1,2 +1,4 @@
 # This is an analysis of Census Block Groups potentially eligible for de-designation under the state's MA EJ policy
 
+# “the secretary may determine that a neighborhood, including any geographic portion thereof, shall not be designated an environmental justice population upon finding that: (A) the annual median household income of that neighborhood is greater than 125 per cent of the statewide median household income; (B) a majority of persons age 25 and older in that neighborhood have a college education; (C) the neighborhood does not bear an unfair burden of environmental pollution; and (D) the neighborhood has more than limited access to natural resources, including open spaces and water resources, playgrounds and other constructed outdoor recreational facilities and venues.”
+
